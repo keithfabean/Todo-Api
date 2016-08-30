9be8ba9d77b4946e12fc932fb7871b047af9dc39
Keith Fabean <keithfabean@gmail.com>
2016-08-30 16:59:44 -0700

    Refactor POST todos

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 
 var PORT = process.env.PORT || 3000;
 
@@ -38,14 +40,8 @@ app.get('/todos', function(req, res){
 //------------------------------------------------------
 app.get('/todos/:id', function(req, res){
   var todoId = parseInt(req.params.id,10);
-  var todoItem;
 
-  todos.forEach(function(todo) {
-
-    if (todoId === todo.id){
-      todoItem = todo;
-    }
-  });
+  var todoItem = _.findWhere(todos, {id: todoId});
 
   if (typeof todoItem === 'undefined'){
     res.status(404).send();
@@ -56,8 +52,22 @@ app.get('/todos/:id', function(req, res){
 //  res.send('Asking for Todo with id of: ' + req.params.id);
 });
 
+//------------------------------------------------------
 app.post('/todos', function(req, res){
-  var body = req.body;
+  //Make sure only valid fields are in the body
+  var body = _.pick(req.body, 'description', 'completed');
+
+// if the completed flag is NOT a boolean value
+//              OR
+// the description is NOT a string
+//Return a status of 400
+
+  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+    return res.status(400).send();
+  }
+
+  //set body.description to trimed value.
+  body.description = body.description.trim();
 
   //add id field to the todolist and increment id
   body.id = todoNextId;
