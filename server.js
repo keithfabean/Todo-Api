46a9ffffe135ec7eab9e2a5d30662d4c52c30be4
Keith Fabean <keithfabean@gmail.com>
2016-09-14 12:12:08 -0700

    Added DELETE /users/login

--- server.js
+++ server.js
@@ -260,20 +260,34 @@ app.post('/user', function(req, res){
 
 //------------------------------------------------------
 app.post('/users/login', function(req, res){
-  //Make sure only valid fields are in the body
-  var body = _.pick(req.body, 'email', 'password');
+    //Make sure only valid fields are in the body
+    var body = _.pick(req.body, 'email', 'password');
+    var userInstance;
 
-  db.user.authenticate(body).then(function (user){
-    var token = user.generateToken('authentication');
-    if (token) {
-      res.header('Auth', token).json(user.toPublicJSON());
-    } else {
-      res.status(401).send('Could not authenticate.');
-    }
-  }, function(err) {
-    res.status(401).send('Could not authenticate.');
-  });
+    db.user.authenticate(body).then(function (user){
+        var token = user.generateToken('authentication');
 
+        userInstance = user;
+
+        return db.token.create({token: token});
+
+    }).then(function(tokenInstance){
+        res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+    }).catch(function(err) {
+        res.status(401).send('Could not authenticate.');
+    });
+
+});
+
+//------------------------------------------------------
+app.delete('/users/login', middleware.requireAuthentication, function(req, res){
+
+	req.token.destroy().then(function () {
+		res.status(204).send();
+	}).catch(function (err) {
+		res.status(500).send();
+	});
+    
 });
 
 //------------------------------------------------------
